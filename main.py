57029ff9ba401d83b195eef5ca38392e867a03e2
Scheisse <jjwazoo@gmail.com>
2021-09-18 22:25:10 -0400

    check empty arg for echo

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ bot = commands.Bot(intents=intents, command_prefix=config['general']['prefix'])
 
 @bot.command(aliases=["test"])
 async def echo(ctx, *arg):
-    await ctx.send(' '.join(arg))
+    await ctx.send(' '.join(arg or "echo"))
 
 @bot.command()
 async def purge(ctx, amount: int):
