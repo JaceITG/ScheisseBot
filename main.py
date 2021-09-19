a8a7da96095171f23ec00f54c3f98324ced83fe1
Scheisse <jjwazoo@gmail.com>
2021-09-18 22:29:56 -0400

    optional convertor for purge amount

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys, os
 
-import discord
+import discord, typing
 from discord.ext import commands
 from configparser import ConfigParser
 from discord import Intents
@@ -14,10 +14,11 @@ bot = commands.Bot(intents=intents, command_prefix=config['general']['prefix'])
 
 @bot.command(aliases=["test"])
 async def echo(ctx, *arg):
-    await ctx.send(' '.join(arg or "echo"))
+    await ctx.send(' '.join(arg) or "echo")
 
 @bot.command()
-async def purge(ctx, amount: int):
+@commands.is_owner
+async def purge(ctx, amount: typing.Optional[int] = 100):
     await ctx.channel.purge(limit=amount)
 
 bot.run(os.getenv('TOKEN'))
