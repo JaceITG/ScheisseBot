0dd82f3c6f5e3f560c8b201f1cbf27eac13c9afc
Scheisse <jjwazoo@gmail.com>
2021-09-20 13:49:00 -0400

    err emb

--- main.py
+++ main.py
@@ -18,11 +18,24 @@ bot = commands.Bot(intents=intents, command_prefix=config['general']['prefix'])
 async def purge(ctx, amount: typing.Optional[int] = 100):
     await ctx.channel.purge(limit=amount)
 
+######## SERVER/META ########
+@bot.command(aliases=['rolerequest','request','rr'])
+async def role(ctx, *args):
+    if len(args)<2:
+        await ctx.send()
+
 ######## MISC ########
 
 @bot.command(aliases=["test"])
 async def echo(ctx, *arg):
     await ctx.send(' '.join(arg) or "echo")
+
+######## HELPER FUNCS ########
+
+async def _err_embed(msg):
+    emb = discord.Embed(description=msg, color=discord.Color.dark_red)
+    return emb
+
 
 bot.run(os.getenv('TOKEN'))
 
