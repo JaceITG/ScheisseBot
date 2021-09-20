47090f44bec0ca2ba34284d6cb63a40ad50d27a5
Scheisse <jjwazoo@gmail.com>
2021-09-20 14:35:17 -0400

    Parse requests for custom role

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
 import sys, os
+from colour import Color
 
 import discord, typing
 from discord.ext import commands
 from configparser import ConfigParser
 from discord import Intents
+from discord.ext.commands.errors import ArgumentParsingError
 intents = Intents.all()
 
 config = ConfigParser()
 config.read('config.ini')
+PREFIX = config['general']['prefix']
 
-bot = commands.Bot(intents=intents, command_prefix=config['general']['prefix'])
+bot = commands.Bot(intents=intents, command_prefix=PREFIX)
+
+@bot.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(bot))
 
 ######## ADMIN ########
 
@@ -21,8 +28,22 @@ async def purge(ctx, amount: typing.Optional[int] = 100):
 ######## SERVER/META ########
 @bot.command(aliases=['rolerequest','request','rr'])
 async def role(ctx, *args):
+
     if len(args)<2:
-        await ctx.send()
+        raise commands.ArgumentParsingError        
+    
+    argstr = ' '.join(args)
+
+    color = await _parse_to_hex(argstr[0:argstr.find(',')])
+    
+    await ctx.send(f"Color: {color} Role Name: {argstr[argstr.find(',')+2:]}")
+
+@role.error
+async def role_error(ctx, error):
+    if isinstance(error, commands.ArgumentParsingError):
+        await _send(ctx, embed=await _err_embed('Usage: {0}role [color], [name]'.format(PREFIX), '{0}role ff1493, Cool Guy\nHex colors must start with #'.format(PREFIX)))
+    else:
+        print(error)
 
 ######## MISC ########
 
@@ -32,10 +53,26 @@ async def echo(ctx, *arg):
 
 ######## HELPER FUNCS ########
 
-async def _err_embed(msg):
-    emb = discord.Embed(description=msg, color=discord.Color.dark_red)
+async def _send(ctx, msg=None, embed=None, file=None):
+    try:
+        await ctx.send(content=msg, embed=embed, file=None)
+    except Exception as e:
+        print('Error while sending message in {0.channel}: {0}'.format(ctx,e))
+
+async def _err_embed(msg, example=None):
+    emb = discord.Embed(description=msg, color=discord.Color.dark_red())
+
+    if example:
+        emb.add_field(name='Example:', value=example)
+
     return emb
 
+async def _parse_to_hex(colorstr):
+    try:
+        c = Color(colorstr)
+    except ValueError as e:
+        raise commands.ArgumentParsingError
+    return c.hex_l
 
 bot.run(os.getenv('TOKEN'))
 
