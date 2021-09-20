69984602f0f32a5db2a2fde7c96c5b200590e731
Scheisse <jjwazoo@gmail.com>
2021-09-20 13:10:26 -0400

    Command handler categorization

--- main.py
+++ main.py
@@ -11,15 +11,18 @@ config.read('config.ini')
 
 bot = commands.Bot(intents=intents, command_prefix=config['general']['prefix'])
 
-
-@bot.command(aliases=["test"])
-async def echo(ctx, *arg):
-    await ctx.send(' '.join(arg) or "echo")
+######## ADMIN ########
 
 @bot.command()
 @commands.is_owner()
 async def purge(ctx, amount: typing.Optional[int] = 100):
     await ctx.channel.purge(limit=amount)
+
+######## MISC ########
+
+@bot.command(aliases=["test"])
+async def echo(ctx, *arg):
+    await ctx.send(' '.join(arg) or "echo")
 
 bot.run(os.getenv('TOKEN'))
 
